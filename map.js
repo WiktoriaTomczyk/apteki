aec872e2a2f999f971f32d105acf093b6eac8fdd
Wiktoria Tomczyk <autoryzowana@gmail.com>
2017-06-27 22:23:08 +0200

    stworzenie mapy

--- map.js
+++ map.js
@@ -1,6 +1,11 @@
 /**
  * Created by Wiktoria on 2017-06-27.
  */
+var map;
 require(["esri/map", "dojo/domReady!"], function(Map) {
-
+    map = new Map("map", {
+        basemap: "topo",
+        center: [51.3649, 22.5],
+        zoom: 13
+    });
 });
