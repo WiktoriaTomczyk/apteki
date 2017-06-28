ae09869bac154b1603adefb5708fc9058dedeede
Wiktoria Tomczyk <autoryzowana@gmail.com>
2017-06-28 21:21:44 +0200

    dodanie legendy do mapy

--- map.js
+++ map.js
@@ -1,7 +1,14 @@
 /**
  * Created by Wiktoria on 2017-06-27.
  */
-require(["esri/map", "esri/arcgis/utils", "dojo/domReady!"], function(Map, arcgisUtils){
+const dijits = [
+    "esri/map",
+    "esri/arcgis/utils",
+    "esri/dijit/Legend",
+    "dojo/domReady!"
+];
+
+require(dijits, function(Map, arcgisUtils, Legend){
     "use strict";
     /*const map = new Map("map",{
         basemap: "osm",
@@ -10,5 +17,11 @@ require(["esri/map", "esri/arcgis/utils", "dojo/domReady!"], function(Map, arcgi
     });*/
     arcgisUtils.createMap("2708931b404c406db558bc0aa8f4ab73", "map").then(function(response){
         const map = response.map;
+
+        const legend = new Legend({
+            map: map,
+            layerInfos: (arcgisUtils.getLegendLayers(response))
+        }, "legend");
+        legend.startup();
     });
 });
