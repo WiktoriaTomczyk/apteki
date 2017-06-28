b04f54101257cfa87383d920fd55db27396f3264
Wiktoria Tomczyk <autoryzowana@gmail.com>
2017-06-28 20:50:22 +0200

    zaladowanie webmapy w map.js

--- map.js
+++ map.js
@@ -1,11 +1,14 @@
 /**
  * Created by Wiktoria on 2017-06-27.
  */
-require(["esri/map", "dojo/domReady!"], function(Map) {
+require(["esri/map", "esri/arcgis/utils", "dojo/domReady!"], function(Map, arcgisUtils){
     "use strict";
-    const map = new Map("map",{
+    /*const map = new Map("map",{
         basemap: "osm",
         center: [22.5, 51.3649],
         zoom: 13
+    });*/
+    arcgisUtils.createMap("2708931b404c406db558bc0aa8f4ab73", "map").then(function(response){
+        const map = response.map;
     });
 });
