3103935ec00a52b955132f8d178fb05b5285ae9a
Wiktoria Tomczyk <autoryzowana@gmail.com>
2017-06-28 20:44:17 +0200

    parse on load dojoconfig; zmiana var map na const map

--- map.js
+++ map.js
@@ -1,9 +1,9 @@
 /**
  * Created by Wiktoria on 2017-06-27.
  */
-var map;
 require(["esri/map", "dojo/domReady!"], function(Map) {
-    map = new Map("map", {
+    "use strict";
+    const map = new Map("map",{
         basemap: "osm",
         center: [22.5, 51.3649],
         zoom: 13
