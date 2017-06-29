e01710b7645b451871724fc208b657a25f9c5712
Wiktoria Tomczyk <autoryzowana@gmail.com>
2017-06-29 11:23:44 +0200

    zalaczenie dojo dla mobile view

--- map.js
+++ map.js
@@ -1,7 +1,29 @@
 /**
  * Created by Wiktoria on 2017-06-27.
  */
-require(["esri/map", "dojo/domReady!"], function(Map) {
+require([
+    "esri/map",
+    "dojox/mobile",
+    "dojox/mobile/parser",
+    "esri/sniff",
+    "dojox/mobile/deviceTheme",
+    "dojo/dom",
+    "dijit/registry",
+    "dojo/on",
+    "dojox/mobile/ToolBarButton",
+    "dojox/mobile/View",
+    "dojox/mobile/ContentPane",
+    "dojo/domReady!"
+], function(
+    Map,
+    mobile,
+    parser,
+    has,
+    dTheme,
+    dom,
+    registry,
+    on
+){
     "use strict";
     const map = new Map("map",{
         basemap: "osm",
