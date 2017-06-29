2384b5a232b5e9f78d65417cc01a0f894c1063d7
Wiktoria Tomczyk <autoryzowana@gmail.com>
2017-06-29 20:09:55 +0200

    zmiana styli na sass

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,9 @@
  */
 const gulp = require("gulp");
 const babel = require("gulp-babel");
+const sass = require("gulp-sass");
 
-gulp.task("default", ["map", "dojoConfig"]);
+gulp.task("default", ["map", "dojoConfig", "style"]);
 
 gulp.task("map", () => {
     return gulp.src("map.js")
@@ -19,5 +20,11 @@ gulp.task("dojoConfig", () => {
         .pipe(babel({
             presets: ["es2015"]
         }))
+        .pipe(gulp.dest("dist"));
+});
+
+gulp.task("style", () => {
+    return gulp.src("style.sass")
+        .pipe(sass().on("error", sass.logError))
         .pipe(gulp.dest("dist"));
 });
