861dd5a7494d36a221da290a60fb2d7b8f35059e
Wiktoria Tomczyk <autoryzowana@gmail.com>
2017-06-29 12:43:15 +0200

    obsluga urzadzen mobilnych

--- map.js
+++ map.js
@@ -25,9 +25,53 @@ require([
     on
 ){
     "use strict";
-    const map = new Map("map",{
+
+    mobile.hideAddressBar();
+
+    let resizeEvt = (
+        window.onorientationchange !== undefined &&
+            !has("android")
+    ) ? "orientationchange" : "resize";
+
+    on(window, resizeEvt, resizeMap);
+
+    const map = new Map("map", {
         basemap: "osm",
         center: [22.5, 51.3649],
-        zoom: 13
+        zoom: 13,
+        slider: false
     });
+
+    map.on("load", function() {
+        resizeMap();
+        registry.byId("mapView").on("AfterTransitionIn", resizeMap);
+    });
+
+    function resizeMap() {
+        mobile.hideAddressBar();
+        adjustMapHeight();
+        map.resize();
+        map.reposition();
+    }
+
+    function adjustMapHeight() {
+        let availHeight = mobile.getScreenSize().h - registry.byId("header").domNode.clientHeight - 1;
+        if (has("iphone") || has("ipod")) {
+            availHeight += iphoneAdjustment();
+        }
+        dom.byId("map").style.height = availHeight + "px";
+    }
+
+    function iphoneAdjustment() {
+        let sz = mobile.getScreenSize();
+        if (sz.h > sz.w) {
+            return screen.availHeight - window.innerHeight - 44;
+        } else {
+            let _conn = on(window, "resize", function () {
+               _conn.remove();
+               resizeMap();
+            });
+            return 0;
+        }
+    }
 });
